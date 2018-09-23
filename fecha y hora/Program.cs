aeda11683eb7e7cd455327c6e0e0a724cd962d3c
Rosa M. Rodriguez <romarola@hotmail.com>
2018-09-22 22:06:03 -0300

    fecha y hora

---
+++ fecha y hora/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace fecha_y_hora
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DateTime fecha = DateTime.Now;   // creo una variable para una nueva fecha
+            Console.WriteLine("Hoy es {0} del mes {1} de {2}",fecha.Day,fecha.Month,fecha.Year);
+            DateTime manyana = fecha.AddDays(1); // comando para aunmentar un dia 
+            Console.WriteLine("Mañana sera {0}", manyana.Day);
+            Console.ReadKey();
+        }
+        
+    }
+}
